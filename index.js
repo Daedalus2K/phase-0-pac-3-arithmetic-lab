54f9b7f60095a5e309c560b24690f09503517d9d
Daedalus2K <apolloswatered@gmail.com>
2023-01-18 01:21:33 -0500

    commit tree 26055c113e59f5d47eadebf77ce30e4f293f3d26

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide(){}
+
+function add(a,b){
+    return  a + b 
+}
+
+function subtract(a,b){
+    return  a - b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function increment(z){
+    return (++z); // or (return a -= )
+}
+increment()
+
+function decrement(q){
+    return (--q); // or (return q -= 1)
+}
+decrement()
+//the test allows you to use any variable letter
+
+function makeInt(y){
+    y === "2"
+    return parseInt( y ,10)
+}
+
+function preserveDecimal(k){
+    k == "3.142"
+    return parseFloat( k, 2)
+}
+
